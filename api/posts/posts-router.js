8dcec9fc698d05423afec4b3953cfc05c1e75b90
Durrell Jules <durrelljules@Durrells-Mini.attlocal.net>
2021-08-07 19:21:28 -0400

    setup server, works as intended

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,15 @@
 // implement your posts router here
+
+// Step 1 : Create new file
+
+
+// Step 2 : Bring in Model and Endpoints
+
+// const Posts = require('./posts-model');
+
+
+// Step 4  - Rename import and bring in express with Router Method
+
+
+// const express = require("express")
+// const router = express.Router()
--- api/server.js
+++ api/server.js
@@ -1,2 +1,32 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+
+
+// Step 3 - Part 2 : Declare new router Variable (postsRouter)
+
+
+
+
+
+
+const server = express();
+
+server.use(express.json());
+
+// Step 3 - Part 1 : tell sever to use the new router (postsRouter)
+
+
+// End Points
+
+// Test endpoint
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Posts Api</h2>
+    <p>Welcome to the Posts Api</p>
+  `);
+});
+
+
+// Export server
+module.exports = server;
