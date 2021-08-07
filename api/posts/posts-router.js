c23be6dd0bf9059450d45b0c0a258bc33755b7df
Durrell Jules <durrelljules@Durrells-Mini.attlocal.net>
2021-08-07 19:48:02 -0400

    created get by id endpoint for posts, works as intended, applied conditional logic if there is no id that matches

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -34,6 +34,26 @@ router.get("/", (req, res) =>{
 })
 
 
+// Get a specific Post by Id 
+router.get("/:id", (req, res) => {
+  Posts.findById(req.params.id)
+    .then(post => {
+      if(post){
+        res.status(200).json(post)
+      }
+      else{
+        res.status(404).json({ message: "The post with the specified ID does not exist"})
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: "The post information could not be retrieved"
+      })
+    })
+})
+
+
 
 
 
